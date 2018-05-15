9be463bc6e166bbf060136b9f9315b989bcb3996
Jean Kévin <kevin.jean@etu.univ-orleans.fr>
2018-05-15 22:35:49 +0200

    add partie

--- bowling/joueur.py
+++ bowling/joueur.py
@@ -1,17 +1,20 @@
 class Joueur:
-    def __init__(self,nom ="Bernard",tab = []):
+    def __init__(self, nom="Bernard", tab=None):
         self.nom = nom
-        self.changer_tableau_score(tab)
+        if tab is None:
+            self.changer_tableau_score([])
+        else:
+            self.changer_tableau_score(tab)
 
-    def ajouter_score_manche(self,tuple):
-        if tuple[0] < 0 or tuple[1] <0 or tuple[0]+tuple[1]>10:
+    def ajouter_score_manche(self, tuple):
+        if tuple[0] < 0 or tuple[1] < 0 or tuple[0] + tuple[1] > 10:
             raise TypeError("tuple faux")
         if len(self.tableau_score) == 12:
             raise TypeError("tab > 12")
-        if len(self.tableau_score) == 11 :
+        if len(self.tableau_score) == 11:
             if self.tableau_score[9][0] != 10 or self.tableau_score[10][0] != 10:
                 raise TypeError("tab>11")
-            if tuple[1]!=0:
+            if tuple[1] != 0:
                 raise TypeError("input error")
         if len(self.tableau_score) == 10:
             if self.tableau_score[9][0] + self.tableau_score[0][1] != 10:
@@ -19,21 +22,21 @@ class Joueur:
         self.tableau_score.append(tuple)
         return
 
-    def changer_tableau_score(self,tab):
+    def changer_tableau_score(self, tab):
         if len(tab) > 12:
             raise TypeError("tab > 12")
-        if len(tab) > 11 and (tab[9][0] != 10 or  tab[10][0] != 10):
+        if len(tab) > 11 and (tab[9][0] != 10 or tab[10][0] != 10):
             raise TypeError("tab > 10")
-        #parcourt du tableau et utilisation de la fonction ajouter_score_manche pour centraliser les regles
+        # parcourt du tableau et utilisation de la fonction ajouter_score_manche pour centraliser les regles
         self.tableau_score = []
         for tuple in tab:
             self.ajouter_score_manche(tuple)
         return
 
     def calculer_score(self):
-        #if len(self.tableau_score) > 12:
+        # if len(self.tableau_score) > 12:
         #    raise TypeError("tab > 12")
-        #if len(self.tableau_score) > 11 and (self.tableau_score[9][0] != 10 or  self.tableau_score[10][0] != 10):
+        # if len(self.tableau_score) > 11 and (self.tableau_score[9][0] != 10 or  self.tableau_score[10][0] != 10):
         #    raise TypeError("tab > 10")
         sum = 0
         multiplicateur1 = 1
@@ -42,8 +45,8 @@ class Joueur:
         nb_tour = 0
         for (x, y) in self.tableau_score:
             nb_tour += 1
-        #    if x < 0 or y < 0 or x + y > 10:
-        #        raise TypeError("value error")
+            #    if x < 0 or y < 0 or x + y > 10:
+            #        raise TypeError("value error")
 
             sum += multiplicateur1 * x + multiplicateur2 * y
 
